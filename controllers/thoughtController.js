600621b52a24d3c185def4527881d505767c1bcf
luccaloopz <luccamartins2012@gmail.com>
2022-05-22 18:35:25 -0700

    added put and delete by id routes for Thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -24,6 +24,54 @@ module.exports = {
             console.log(err);
             return res.status(500).json(err);
         });
-    }
+    },
 
+    createThought(req, res) {
+        Thought.create(req.body)
+        .then(thought => {
+
+        })
+    },
+
+    updateThought(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+        .then(thought => {
+            !thought 
+                ? res.status(404).json({ message: 'No thought with that ID' })
+                :  res.json(thought)
+        })
+        .catch(err => res.status(500).json(err));
+    },
+
+    deleteThought(req, res) {
+        Thought.findOneAndDelete({ _id: req.params.thoughtId })
+            .then(thought =>
+                !thought
+                    ? res.status(404).json({ message: 'No thought with that ID' })
+                    : User.findOneAndUpdate(
+                        { thoughts: req.params.thoughtId },
+                        { $pull: { thoughts: req.params.thoughtId } },
+                        { new: true }
+                    )
+            )
+            .then(user =>
+                !user
+                    ? res.status(404).json({
+                        message: 'Thought deleted, but no users found',
+                    })
+                    : res.json({ message: 'Thought successfully deleted' })
+            )
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+
+    
+    
+ 
 };
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -3,13 +3,17 @@ const router = require('express').Router();
 const {
     //this is where I'll put all of the methods within thoughtController.js
     getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
 
 
 } = require('../../controllers/thoughtController');
 
-router.route('/').get(getThoughts).post();
+router.route('/').get(getThoughts).post(createThought);
 
-router.route('/:thoughtId').get().put().delete();
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
 router.route('/:thoughtId/reactions').post().delete();
 
